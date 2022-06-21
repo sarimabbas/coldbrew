cef1fc227daf3cc45062b1518ab767a1071b8515
Sarim Abbas <sarim.stays@gmail.com>
2022-06-20 21:27:38 -0700

    update link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
 import React from "react";
 import SessionPill from "./SessionPill";
 
 const Footer = () => {
   return (
     <div className="flex items-center justify-between p-4 flex-wrap">
-      <a href="/about" className="underline">
+      <Link href="/about" className="underline">
         About
-      </a>
+      </Link>
       <SessionPill />
     </div>
   );
