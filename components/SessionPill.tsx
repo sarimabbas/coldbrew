1016c611222e110908a0e49b2bbd84964c5727c8
Sarim Abbas <sarim.stays@gmail.com>
2022-06-20 21:24:05 -0700

    add about page

--- components/SessionPill.tsx
+++ components/SessionPill.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useSession } from "../lib/useSession";
+import cx from "classnames";
 
-const SessionPill = () => {
+interface ISessionPillProps {
+  className?: string;
+}
+const SessionPill = ({ className }: ISessionPillProps) => {
   const { session } = useSession();
 
   if (!session?.id) return null;
 
   return (
-    <div className="surface edges chisel text-sm px-2 py-1">
+    <div className={cx("surface edges chisel text-sm px-2 py-1", className)}>
       Session ...{session.id.slice(-10)}
     </div>
   );
---
+++ pages/about.tsx
@@ -0,0 +1,75 @@
+import Head from "next/head";
+import React from "react";
+import SessionPill from "../components/SessionPill";
+import { useSession } from "../lib/useSession";
+
+const About = () => {
+  const { session } = useSession();
+  return (
+    <div className="container mx-auto p-4 prose prose-sm">
+      <Head>
+        <title>Coldbrew - About</title>
+      </Head>
+      <h3>About Coldbrew</h3>
+      <p>
+        Coldbrew is a hobby project by me, @sarimabbas 👋. For the longest time,
+        I&apos;ve wanted a visual, one-click tool to install all the apps I
+        want.
+      </p>
+      <p>
+        So I built Coldbrew! You can select all the apps you want, and Coldbrew
+        will install them with Homebrew Cask. This is better than going to each
+        app&apos;s website separately and starting all the downloads manually.
+      </p>
+      <p>
+        This does mean that Homebrew is a prerequisite. You can install it on
+        your macOS machine here:{" "}
+        <a href="https://brew.sh/" target="_blank" rel="noreferrer">
+          {" "}
+          https://brew.sh/
+        </a>
+      </p>
+      <p>
+        Coldbrew does nothing fancy, it just invokes <code>brew bundle</code>.
+        Now, you <i>could</i> just write out your desired casks in a dotfile and
+        sync that between computers, but I think there is something fun about
+        browsing through the most popular apps in a visual way. In fact, I
+        provide a way for you to download your selection as a{" "}
+        <code>Brewfile</code>, if you so desire.
+      </p>
+      <h3>Security</h3>
+      <p>Just a little note about security, if you are curious.</p>
+      <p>
+        I don&apos;t require login to use Coldbrew. Instead, I create an
+        anonymous session for each user.
+      </p>
+      {session?.id && (
+        <>
+          <p>Here is yours:</p>
+          <SessionPill className="w-fit" />
+        </>
+      )}
+      <p>
+        Sessions are immutable once you end your Coldbrew browser session. Then
+        you can&apos;t add or remove apps from them any more.
+      </p>
+      <p>
+        You can share the link to your session with someone else. When they
+        visit the share link, every app linked to your session will be copied
+        over to a new session for them.
+      </p>
+      <p>
+        Oh also, you should know that I collect page-view data with Splitbee
+        analytics.
+      </p>
+      <h3>Built with</h3>
+      <p>
+        Coldbrew was built with the Homebrew API, the results of which are
+        stored in a PlanetScale database. The data is queried by a NextJS app
+        with React Query and TRPC. The app is styled with Tailwind.
+      </p>
+    </div>
+  );
+};
+
+export default About;
