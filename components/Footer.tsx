ceedb00414a009b0eafa8185e55d3c6066686f57
Sarim Abbas <sarim.stays@gmail.com>
2022-06-20 21:25:41 -0700

    about

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,10 @@ import SessionPill from "./SessionPill";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center my-4">
+    <div className="flex items-center justify-between p-4 flex-wrap">
+      <a href="/about" className="underline">
+        About
+      </a>
       <SessionPill />
     </div>
   );
