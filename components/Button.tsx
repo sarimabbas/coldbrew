2d6ac130123df92fd1a9f82a7689c5aac338fa07
Sarim Abbas <sarim.stays@gmail.com>
2022-06-21 19:12:01 -0700

    forward ref button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import cx from "classnames";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button: React.FC<ButtonProps> = ({
-  disabled,
-  children,
-  ...delegated
-}) => {
+const Button: React.FC<ButtonProps> = forwardRef<
+  HTMLButtonElement,
+  ButtonProps
+>(({ disabled, children, ...delegated }, ref) => {
   return (
     <button
       {...delegated}
+      ref={ref}
       className={cx("relative p-2 surface edges chisel", {
         "opacity-50 cursor-not-allowed": disabled,
       })}
@@ -18,6 +18,8 @@ const Button: React.FC<ButtonProps> = ({
       {children}
     </button>
   );
-};
+});
+
+Button.displayName = "Button";
 
 export default Button;
